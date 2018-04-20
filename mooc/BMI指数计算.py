a140d8c4c3080d330d19e86090b6fca93d76319a
醉梦半醒 <zmbxzrq@outlook.com>
2018-04-20 09:01:31 +0800

    upload

---
+++ mooc/BMI指数计算.py
@@ -0,0 +1,17 @@
+h,w=eval(input("input"))
+bmi=w/pow(h,2)
+print("BMI指数为{:2f}".format(bmi))
+who,nat="",""
+if(bmi<18.5):
+	who,nat="偏瘦","偏瘦"
+elif 18.5<=bmi<24:
+	who,nat="正常","正常"
+elif 24<=bmi<25:
+	who,nat="正常","偏胖"
+elif 25<=bmi<28:
+	who,nat="偏胖","偏胖"
+elif 28<=bmi<30:
+	who,nat="偏胖","肥胖"
+print("BMI指数为:国际'{}',国内'{}'".format(who,nat))
+
+
