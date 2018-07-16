1fa0b51a5ebb38a1ce3f6ed06c0e61c8ce4ed9ce
zmbxzrq <zmbxzrq@outlook.com>
2018-07-16 11:34:09 +0800

    upload

--- mooc/凯撒解密.py
+++ mooc/凯撒解密.py
@@ -5,8 +5,8 @@ l=len(c)
 for i in range(l):
         if c[i] in ['x','y','z']:
                 s=s+chr(ord(c[i])-23)
-        elif c[i]==' ':
-                s=s+' '
+        elif ord(c[i])<65 or 90<ord(c[i])<97 or ord(c[i])>122:
+          		s=s+c[i]
         else:
                 s=s+chr(ord(c[i])+3)
 print(s)
